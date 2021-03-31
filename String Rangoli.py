7a35d636342fcca1ffe4b7fd1f5a2108be19831c
Badri Gs <12196466+badrigs999@users.noreply.github.com>
2021-03-31 17:12:34 +0530

    Create String Rangoli.py

---
+++ String Rangoli.py
@@ -0,0 +1,20 @@
+import string
+import sys
+def print_rangoli(s):
+
+    alpha = string.ascii_lowercase
+
+    for i in range(s - 1, 0, -1):
+        r = ["-"] * (s * 2 - 1)
+        for j in range(0, s - i):
+            r[s - 1 - j] = alpha[j + i]
+            r[s - 1 + j] = alpha[j + i]
+        print("-".join(r))
+
+    for i in range(0, s):
+        r = ["-"] * (s * 2 - 1)
+        for j in range(0, s - i):
+            r[s - 1 - j] = alpha[j + i]
+            r[s - 1 + j] = alpha[j + i]
+        print("-".join(r))
+print_rangoli(5)
